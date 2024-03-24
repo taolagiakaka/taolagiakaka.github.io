2c5e566d4a0a3c556dd0fffcc9f49fd695b0dbc2
Gia <37612787+taolagiakaka@users.noreply.github.com>
2024-03-24 21:42:31 +0700

    update code

--- app/page.tsx
+++ app/page.tsx
@@ -1,22 +1,23 @@
 export default function Home() {
     return (
         <>
-            <div className="h-screen bg-[#f5df4e] flex flex-col items-center justify-center">
-                <div className="bg-white rounded-full p-2 mb-4 shadow-sm ">
+            <div className="h-screen bg-primary flex flex-col items-center justify-center">
+                <div className="bg-white rounded-full p-2 shadow-sm mb-10">
                     <img
                         height={163}
                         width={162}
                         className="block rounded-full"
                         src="https://cdn.sforum.vn/sforum/wp-content/uploads/2023/11/avatar-dep-105.jpg"
-                        alt=""
+                        alt="Profile Picture"
                     />
                 </div>
-                <h2 className="uppercase font-semibold	text-4xl mb-10">
-                    HELLO! I'M Gia - GRAPHIC DESIGNER, ILLUSTRATOR & ART
-                    DIRECTOR.
+                <h2 className="uppercase font-semibold text-4xl mb-10 text-center">
+                    Hello! I'm Nguyen Truong Gia -<br /> .NET Developer
                 </h2>
-                <p className="text-2xl mb-10">from in TPHCM, VietNam.</p>
-                <button className="rounded-none bg-[#212529] text-white py-4 px-10 font-medium outline-none">
+                <p className="text-2xl mb-10">
+                    Based in Ho Chi Minh City, Vietnam.
+                </p>
+                <button className="rounded-none bg-gray-800 text-white py-4 px-10 font-medium outline-none">
                     About Me
                 </button>
             </div>
