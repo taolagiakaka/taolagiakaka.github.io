6d4fb9847632f9381fc05b268d3216989055d04e
Gia <37612787+taolagiakaka@users.noreply.github.com>
2024-03-25 16:23:50 +0700

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    output: "export", 
+    output: {
+        path: "export",
+    },
     reactStrictMode: true,
 };
-
 export default nextConfig;
